7499e2c7e183cfce57448ec8d318b3ec1b3c45d6
Ana <anais.tin.amara@gmail.com>
2022-03-08 17:17:43 +0100

    first commit

--- app/src/main/java/com/example/sneakerdex/MainActivity.kt
+++ app/src/main/java/com/example/sneakerdex/MainActivity.kt
@@ -4,8 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val firstFragment=HomeFragment()
+    val secondFragment=SecondFragment()
+    val thirdFragment=ThirdFragment()
+
+    setCurrentFragment(firstFragment)
+
+    bottomNavigationView.setOnNavigationItemSelectedListener {
+        when(it.itemId){
+            R.id.home->setCurrentFragment(firstFragment)
+            R.id.person->setCurrentFragment(secondFragment)
+            R.id.settings->setCurrentFragment(thirdFragment)
+
+        }
+        true
+    }
+
 }
